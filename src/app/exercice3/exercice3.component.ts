fce7d443ac1fbb0295792d534b6eaf8b285fc385
d.chabbert <d.chabbert@ediser.com>
2021-04-02 16:32:03 +0200

    Add fonctionality:  switch betwen 2 pargraph

--- src/app/exercice3/exercice3.component.ts
+++ src/app/exercice3/exercice3.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice3',
-  templateUrl: './exercice3.component.html',
-  styleUrls: ['./exercice3.component.css']
+  selector: "app-exercice3",
+  templateUrl: "./exercice3.component.html",
+  styleUrls: ["./exercice3.component.css"],
 })
 export class Exercice3Component implements OnInit {
+  public statut: boolean;
+  public titleButton: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.statut = true;
+    this.titleButton = "p2";
   }
 
+  ngOnInit(): void {}
+
+  public switchStatut(): void {
+    this.statut = !this.statut;
+    this.statut ? (this.titleButton = "p2") : (this.titleButton = "p1");
+  }
 }
